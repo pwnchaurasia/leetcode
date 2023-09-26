d85f1fd0a7028faa67835c48d77d1f8e858c2e62
Pawan <rjsnh1522@gmail.com>
2023-09-26 22:47:01 +0530

    Day 12: 134_Gas_Station : 26/sept/2023

--- Readme.md
+++ Readme.md
@@ -44,4 +44,5 @@ Remember, the 75 Hard Challenge is about personal growth and discipline. Stay co
 - Day 10: [275. H-Index II](https://leetcode.com/problems/h-index-ii/)
 - Day 10: [380. Insert Delete GetRandom O(1)](https://leetcode.com/problems/insert-delete-getrandom-o1/)
 - Day 11: [238. Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self/)
+- Day 12: [134. Gas Station](https://leetcode.com/problems/gas-station/)
 
---
+++ top_interview_questions/75_hard/134_Gas_Station.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total = 0
+        res = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                res = i+1
+        return res
+        
+
+
+gas = [1,2,3,4,5]
+cost = [3,4,5,1,2]
+sol = Solution()
+print(sol.canCompleteCircuit(gas, cost))
+
+
+# gas = [4,5,1,2,3,], cost = [2,3,4,5,1]
