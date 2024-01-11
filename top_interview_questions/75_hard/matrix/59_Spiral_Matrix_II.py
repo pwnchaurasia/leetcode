38ff198c96f9a93671e4c2c4d8e52a2bd37e9972
Pawan <rjsnh1522@gmail.com>
2024-01-11 09:56:01 +0530

    unsolved question commit

---
+++ top_interview_questions/75_hard/matrix/59_Spiral_Matrix_II.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        
+        left = 0
+        right = n
+        top = 0
+        bottom = 0
+
+
