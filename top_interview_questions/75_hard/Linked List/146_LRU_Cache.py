1b8c3f75b5c327a63b3a75e6e82c6c388a945d9b
pawan <rjsnh1522@gmail.com>
2024-03-01 23:24:17 +0530

    Day 15: Attempt 2: 146_LRU_Cache

--- top_interview_questions/75_hard/Linked List/146_LRU_Cache.py
+++ top_interview_questions/75_hard/Linked List/146_LRU_Cache.py
@@ -6,16 +6,18 @@ class LRUCache:
         self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.cache[key]
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
             return self.cache[key]
-        return -1
 
 
     def put(self, key: int, value: int) -> None:
-        if len(self.cache) > self.capacity:
-            val = self.cache.pop()
         self.cache[key] = value
+        self.cache.move_to_end(key)
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last = False)
 
 
 
