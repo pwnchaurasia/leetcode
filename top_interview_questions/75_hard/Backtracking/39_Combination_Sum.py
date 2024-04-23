06fab7efb20fa2172b144fdcb40ab00626eab8ca
Pawan <rjsnh1522@gmail.com>
2024-04-23 18:41:19 +0530

    Day 26 Backtracking

---
+++ top_interview_questions/75_hard/Backtracking/39_Combination_Sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+
+        def dfs(i, current, total):
+            if total == target:
+                result.append(current.copy())
+                return 
+            if total > target or i >= len(candidates):
+                return 
+
+            current.append(candidates[i])
+            dfs(i, current, total+candidates[i])
+            current.pop()
+            dfs(i+1, current, total)
+        dfs(0, [], 0)
+        return result
