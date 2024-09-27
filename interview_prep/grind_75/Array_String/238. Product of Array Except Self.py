cb2617eab671df3a6e12f307eb6d21096562ab02
Pawan <rjsnh1522@gmail.com>
2024-09-27 13:26:33 +0530

    Grind 75: Day 5

--- interview_prep/grind_75/Array_String/238. Product of Array Except Self.py
+++ interview_prep/grind_75/Array_String/238. Product of Array Except Self.py
@@ -1,6 +1,8 @@
 from typing import List
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+
         prefix = 1
         length = len(nums)
         ans = [1] * length
@@ -9,14 +11,15 @@ class Solution:
             prefix *= nums[i]
 
         postfix = 1
-        for i in range(length-1, -1, -1):
+        for i in range(length - 1, -1, -1):
             ans[i] *= postfix
             postfix *= nums[i]
         return ans
+
+
 
 
 if __name__ == "__main__":
     sol = Solution()
     nums = [1, 2, 3, 4]
     print(sol.productExceptSelf(nums=nums))
-
