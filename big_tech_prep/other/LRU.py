77ccecf62159a69854ce26a105ff26755c6dd92f
pawan <rjsnh1522@gmail.com>
2025-09-07 09:53:27 +0530

    Big tech Prep

---
+++ big_tech_prep/other/LRU.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, key=0, val=0):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}  # key -> node
+        # dummy head/tail
+        self.head, self.tail = Node(), Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _add(self, node):
+        # always add right after head
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self._remove(node)
+        self._add(node)
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self.cache[key] = node
+        self._add(node)
+
+        if len(self.cache) > self.cap:
+            # remove from tail
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
